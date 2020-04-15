1409403a41bd58576b7cec49d8b32d84e7ff8575
Robert Helmer <rhelmer@rhelmer.org>
2020-04-15 12:46:07 -0700

    do not bother with pop-up, just use browserAction to trigger bg script.

---
+++ background.js
@@ -0,0 +1,25 @@
+async function submitTelemetry() {
+  if (!(await browser.telemetry.canUpload())) {
+    throw "Telemetry upload is disabled.";
+  }
+
+  browser.telemetry.useEncryption = true;
+
+  const pingType = "pioneer-v2-study";
+  const payload = { pioneerId: "my-pioneer-id" };
+  const options = { useEncryption: true };
+
+  return await browser.telemetry.submitPing(pingType, payload, options);
+}
+
+browser.browserAction.onClicked.addListener(() => {
+  if (browser.telemetry) {
+    submitTelemetry()
+      .then((result) => console.info(`Telemetry submission finished.`))
+      .catch((error) => console.error(`Telemetry submission failed: ${error}`));
+  } else {
+    console.error(
+      "No browser.telemetry API available, is this extension privileged?"
+    );
+  }
+});
