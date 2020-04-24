78b0a70428f5452cf7867bb17cb9440031f587ca
Robert Helmer <rhelmer@rhelmer.org>
2020-04-24 11:22:31 -0700

    remove module, move metadata to manifest and use  simpler API now exposed from Firefox.

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
-import { submitPing } from "./pioneer-telemetry.js";
-
 browser.browserAction.onClicked.addListener(async () => {
   const payload = { key1: "value1", key2: "value2" };
-  await submitPing(payload);
+  await browser.telemetry.submitEncryptedPing(payload);
   console.info("Telemetry submitted, check about:telemetry");
 });
