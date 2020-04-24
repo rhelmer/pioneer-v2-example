2f4fcf5cdd08525383e929c8b9f868973282b077
Robert Helmer <rhelmer@rhelmer.org>
2020-04-24 15:42:35 -0700

    no longer need the background html if we are not using modules.

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 browser.browserAction.onClicked.addListener(async () => {
-  const payload = { key1: "value1", key2: "value2" };
+  const payload = {
+    key1: "value1",
+    key2: "value2",
+  };
   await browser.telemetry.submitEncryptedPing(payload);
   console.info("Telemetry submitted, check about:telemetry");
 });
