52618c0155a4b5b83c52b9e0b47aafbfe82fd97b
Robert Helmer <rhelmer@rhelmer.org>
2020-05-27 11:27:47 -0700

    make eslint happy

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
+// eslint-disable-next-line no-undef
 browser.browserAction.onClicked.addListener(async () => {
   const currentDate = new Date();
 
   const payload = {
-    "datetime": currentDate
-  }
-
-  const options = {
-    "schemaName": "debug",
-    "schemaVersion": 1
+    datetime: currentDate
   };
 
+  const options = {
+    schemaName: "debug",
+    schemaVersion: 1
+  };
+
+  // eslint-disable-next-line no-undef
   await browser.telemetry.submitEncryptedPing(payload, options);
   console.info("Telemetry submitted, check about:telemetry");
 });
