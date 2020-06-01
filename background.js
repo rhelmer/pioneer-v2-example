a630c6dfc22d2d7aaeaa6833440ae7f1ea2b76a6
Robert Helmer <rhelmer@rhelmer.org>
2020-06-01 08:31:47 -0700

    remove toolbar button, send encrypted ping immediately

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
+const currentDate = new Date();
+
+const payload = {
+  datetime: currentDate,
+};
+
+const options = {
+  schemaName: "debug",
+  schemaVersion: 1,
+};
+
 // eslint-disable-next-line no-undef
-browser.browserAction.onClicked.addListener(async () => {
-  const currentDate = new Date();
-
-  const payload = {
-    datetime: currentDate
-  };
-
-  const options = {
-    schemaName: "debug",
-    schemaVersion: 1
-  };
-
-  // eslint-disable-next-line no-undef
-  await browser.telemetry.submitEncryptedPing(payload, options);
-  console.info("Telemetry submitted, check about:telemetry");
-});
+browser.telemetry.submitEncryptedPing(payload, options).then(
+  result => {
+    console.info("Telemetry submitted, check about:telemetry");
+  },
+  error => {
+    console.error("Unable to send encrypted telemetry:", error);
+  }
+);
