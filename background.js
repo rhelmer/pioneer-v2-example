e49c949d960595791ecd4c7006a85d704b41a1b1
Robert Helmer <rhelmer@mozilla.com>
2020-06-05 12:29:55 -0700

    Remove toolbar button (#8)
    * open foreground tab instead of setUninstallURL
    
    * remove mgmnt permission for now
    
    * correct syntax

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 async function sendPing() {
   const currentDate = new Date();
 
@@ -10,7 +11,6 @@ async function sendPing() {
     schemaVersion: 1
   };
 
-  // eslint-disable-next-line no-undef
   await browser.telemetry.submitEncryptedPing(payload, options);
 }
 
@@ -22,14 +22,9 @@ sendPing()
   )
   .catch(error => console.error("Could not send ping:", error));
 
-// eslint-disable-next-line no-undef
-browser.runtime.setUninstallURL("https://forms.gle/fLwAS3YUmPm3Qx959");
-
-// eslint-disable-next-line no-undef
-browser.management
-  .uninstallSelf()
-  .then(result =>
-    console
-      .info("Uninstalled Pioneer v2 demo study")
-      .catch(error => console.error("Could not uninstall self:", error))
-  );
+browser.tabs
+  .create({
+    url: "https://forms.gle/fLwAS3YUmPm3Qx959"
+  })
+  .then(result => console.info("Uninstalled Pioneer v2 demo study"))
+  .catch(error => console.error("Could not uninstall self:", error));
